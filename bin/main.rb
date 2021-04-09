960ae561e81f702b1ee5081c3a005c0374ecb99f
crappypapa <devsolaoluwa@gmail.com>
2021-04-10 00:02:41 +0100

    Add functionality methods

--- bin/main.rb
+++ bin/main.rb
@@ -3,17 +3,20 @@ require_relative '../lib/scraper'
 
 puts 'Hello there'
 puts 'Welcome to Crappy\'s StackOverflow job Scraper'
-puts 
-puts "What is your name?"
+puts
+puts 'What is your name?'
 name = gets.chomp
 puts
 puts "#{name}, Have a donut while I scrape the Job listings on Stack-Overflow"
 puts
 puts
-puts "scraping..."
-puts "scraping..."
-puts "scraping..."
+puts 'scraping...'
+puts 'scraping...'
+puts 'scraping...'
 
+site = Scraper.new
+site.scrape
+site.result
 puts
 puts
 puts 'Scraping successfully completed'
