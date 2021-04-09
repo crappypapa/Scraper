2cfb4538e78386e9df292c5fb25e1e3a347f5cc0
crappypapa <devsolaoluwa@gmail.com>
2021-04-09 23:15:46 +0100

    Add User-interface

---
+++ bin/main.rb
@@ -0,0 +1,20 @@
+#!usr/bin/env ruby
+require_relative '../lib/scraper'
+
+puts 'Hello there'
+puts 'Welcome to Crappy\'s StackOverflow job Scraper'
+puts 
+puts "What is your name?"
+name = gets.chomp
+puts
+puts "#{name}, Have a donut while I scrape the Job listings on Stack-Overflow"
+puts
+puts
+puts "scraping..."
+puts "scraping..."
+puts "scraping..."
+
+puts
+puts
+puts 'Scraping successfully completed'
+puts 'The results are in the "StackOverflow.csv" file'
