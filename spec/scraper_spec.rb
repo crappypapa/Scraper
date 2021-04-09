82f688d1e6ef2a269330a7ae3ddf5183375bc233
crappypapa <devsolaoluwa@gmail.com>
2021-04-09 23:47:08 +0100

    Add test for result method

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -12,4 +12,18 @@ describe Scraper do
       expect(site.jobs).to be_a(Array)
     end
   end
+
+  
+
+  describe '#result' do
+    filepath = "StackOverflow.csv"
+    csv_options = {headers: :first_row, col_sep: ','}
+    let(:csv) { CSV.open(filepath, 'wb', csv_options) }
+    let(:update_csv) { csv <<[a[:job_title], a[:job_company], a[:job_time_posted]] }
+
+    it 'creates and updates csv sheet' do
+      site.result
+      expect(csv).to_not be_nil
+    end
+  end
 end
