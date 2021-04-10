1c44066d82e5e476cc0eb952bd161f5c87519e97
crappypapa <devsolaoluwa@gmail.com>
2021-04-10 17:48:41 +0100

    Refactor interface to accept and validate query

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 require_relative '../lib/scraper'
 require_relative '../lib/user'
+require_relative '../lib/sort'
 
 puts 'Hello there'
 puts 'Welcome to Crappy\'s StackOverflow job Scraper'
@@ -14,16 +15,37 @@ while validation == false
   p 'Please, enter a valid name' if validation == false
 end
 puts
-puts "#{name}, Have a donut while I scrape the Job listings on Stack-Overflow"
+puts
+sort = Sorter.new
+puts 'By what query would you like your search to be done?'
+puts
+puts
+puts '"Title" for Job title || "Company" for Job company || "Tag" for Language tag ||"None" for all job listings.'
+selection = false
+while selection == false
+  option = gets.chomp.downcase
+  selection = true if option == "none"
+  if sort.option_check(option) 
+    puts "What #{option} do you want to search for?"
+    @query = gets.chomp.downcase
+  end
+  selection = sort.option_check(option)
+  p 'Please, select one of the options listed above' if selection == false
+end
+puts
+puts
+puts 'Alright!'
+puts "#{name}, Have a donut while I scrape the Job listings on Stack-Overflow based on your query"
 puts
 puts
 puts 'scraping...'
 puts 'scraping...'
 puts 'scraping...'
-site = Scraper.new
-site.scrape
-site.result
+# site = Scraper.new
+# site.scrape
+# site.result
+# puts
 puts
-puts
+
 puts 'Scraping successfully completed'
 puts 'The results are in the "StackOverflow.csv" file'
