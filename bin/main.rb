632651c898947fb3f792569575ebb8dff5355656
crappypapa <devsolaoluwa@gmail.com>
2021-04-10 15:07:02 +0100

    Update code to allow name validation

--- bin/main.rb
+++ bin/main.rb
@@ -1,11 +1,18 @@
 #!/usr/bin/env ruby
 require_relative '../lib/scraper'
+require_relative '../lib/user'
 
 puts 'Hello there'
 puts 'Welcome to Crappy\'s StackOverflow job Scraper'
 puts
+user = User.new
 puts 'What is your name?'
-name = gets.chomp
+validation = false
+while validation == false
+  name = gets.chomp
+  validation = user.validate(name)
+  p 'Please, enter a valid name' if validation == false
+end
 puts
 puts "#{name}, Have a donut while I scrape the Job listings on Stack-Overflow"
 puts
