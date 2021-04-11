161732822e95d89db7ba3ee47a0b334bf04a2f58
crappypapa <devsolaoluwa@gmail.com>
2021-04-11 08:42:41 +0100

    Add method to find job based on query

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,13 +1,19 @@
 require 'nokogiri'
 require 'open-uri'
 require 'csv'
+#require_relative '../bin/main'
+
 
 class Scraper
-  attr_reader :jobs, :url
+  attr_reader :jobs, :url, :query, :option
 
   def initialize
     @jobs = []
+    @option = option
+    @job_sorted = Array.new
     @url = 'https://stackoverflow.com/jobs'
+    @query = query
+    @options = ['title', 'company','tag']
   end
 
   def scrape
@@ -29,15 +35,40 @@ class Scraper
     end
   end
 
+  
+  
+  def option_check(option)
+    @options.include? option
+  end
+
+  def query_title(query)
+   @jobs.select {|a| a[:job_title].downcase.match(/#{Regexp.quote("#{query}")}/)}
+  end
+ 
+  def query_tag(query)
+   @jobs.select {|a| a[:job_tag].downcase.match(/#{Regexp.quote("#{query}")}/)}
+  end
+
+  def query_company(query)
+   @jobs.select {|a| a[:job_company].downcase.match(/#{Regexp.quote("#{query}")}/)}
+  end
+
+
   def result
-    jobs = @jobs
+    job_sorted = @job_sorted.flatten
     filepath = 'StackOverflow.csv'
     csv_options = { headers: :first_row, col_sep: ',' }
-    CSV.open(filepath, 'wb', csv_options) do |csv|
-      csv << ['Title','Job_Tag' ,'Company', 'Time Posted']
-      jobs.each do |a|
-        csv << [a[:job_title],a[:job_tag], a[:job_company], a[:job_time_posted]]
+    if job_sorted.length > 0
+      CSV.open(filepath, 'wb', csv_options) do |csv|
+        csv << ['Title','Job_Tag' ,'Company', 'Time Posted']
+        job_sorted.each do |a|
+          csv << [a[:job_title],a[:job_tag], a[:job_company], a[:job_time_posted]]
+        end
       end
+      puts 'Scraping successfully completed'
+      puts 'The results are in the "StackOverflow.csv" file'
+    else
+      puts "I am unable to find valid matches at this time, Try again later."
     end
   end
 
